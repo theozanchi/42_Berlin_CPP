89ddb8731136e3694aabe9f86890a200f732c57c
tzanchi <tzanchi@student.42berlin.de>
2023-10-20 13:29:33 +0200

    Module00_ex00 init

---
+++ Module_00/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/20 12:03:27 by tzanchi           #+#    #+#             */
+/*   Updated: 2023/10/20 13:27:56 by tzanchi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
