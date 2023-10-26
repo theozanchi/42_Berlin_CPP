78cf4774dc038053f9c8345837af5ed00bad9c01
tzanchi <tzanchi@student.42berlin.de>
2023-10-26 18:18:27 +0200

    module 01 ex02

---
+++ Module_01/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 18:09:36 by tzanchi           #+#    #+#             */
+/*   Updated: 2023/10/26 18:18:13 by tzanchi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main (void)
+{
+	std::string		string = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &string;
+	std::string		&stringREF = string;
+
+	std::cout << &string << std::endl;
+	std::cout << &stringPTR << std::endl;
+	std::cout << &stringREF << std::endl << std::endl;
+
+	std::cout << string << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+}
