06a817c00665c746d5aa2a7a812a3e61a04c023b
tzanchi <tzanchi@student.42berlin.de>
2023-12-05 12:17:30 +0100

    syntax modif + flag added

--- Module_01/ex01/main.cpp
+++ Module_01/ex01/main.cpp
@@ -6,26 +6,27 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 12:20:40 by tzanchi           #+#    #+#             */
-/*   Updated: 2023/12/04 20:56:07 by tzanchi          ###   ########.fr       */
+/*   Updated: 2023/12/05 12:16:23 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+#include <cstdlib>
 
 Zombie*	zombieHorde( int N, std::string name );
 
 int	main( int argc, char **argv )
 {
 	if (argc != 3)
-		return (EXIT_FAILURE);
+		return (1);
 
-	size_t	N = atoi(argv[1]);
+	int	N = std::atoi(argv[1]);
 	Zombie	*horde = zombieHorde(N, argv[2]);
 
-	for (size_t i = 0; i < N; ++i)
+	for (int i = 0; i < N; ++i)
 		horde[i].announce();
 
-	for (size_t i = 0; i < N; ++i)
+	for (int i = 0; i < N; ++i)
 		horde[i].~Zombie();
 
 	operator delete[](horde);
