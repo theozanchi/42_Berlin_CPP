ced1ac6143c2f02cf7051be3bd9f03acd3884302
tzanchi <tzanchi@student.42berlin.de>
2023-12-07 09:46:51 +0100

    final fix of m_02 ex02

--- Module_02/ex03/bsp.cpp
+++ Module_02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 19:34:39 by tzanchi           #+#    #+#             */
-/*   Updated: 2023/12/07 09:39:25 by tzanchi          ###   ########.fr       */
+/*   Updated: 2023/12/07 09:46:15 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,12 @@ bool	same_half_plan(Point const a, Point const b, Point const m, Point const n)
 	Fixed value1 = A * m.getX() + B * m.getY() + C;
 	Fixed value2 = A * n.getX() + B * n.getY() + C;
 
+	if (value1 == 0 || value2 == 0)
+		return (false);
+
 	bool	sign1 = same_sign(value1, Fixed( 1 ));
 	bool	sign2 = same_sign(value2, Fixed( 1 ));
-	
+
 	if ((sign1 && sign2) || (!sign1 && !sign2))
 		return (true);
 	else
