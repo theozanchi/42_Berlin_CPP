9fbc86134b1f07cada6c7f32475a062b1acc8442
tzanchi <tzanchi@student.42berlin.de>
2024-02-19 19:32:54 +0100

    forgot exit_failure code

--- Module_09/ex01/src/main.cpp
+++ Module_09/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 16:49:16 by tzanchi           #+#    #+#             */
-/*   Updated: 2024/02/19 18:57:10 by tzanchi          ###   ########.fr       */
+/*   Updated: 2024/02/19 19:32:37 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	main( int argc, char **argv ) {
 	if (argc != 2) {
 		std::cerr << "Error, one arg string only" << std::endl;
 		std::cerr << "Ex: \"1 2 + 3 4 - 5 6 * 7 8 /\"" << std::endl;
+		return (EXIT_FAILURE);
 	}
 	else {
 		try {
